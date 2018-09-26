1534bff2b0568d52383f715f6d1f1f2d9b5a3660
Dan <daniel.ddsc@gmail.com>
2018-09-26 16:19:28 -0300

    Use InputSearch from vtex.styleguide

--- react/components/SearchMap.js
+++ react/components/SearchMap.js
@@ -5,6 +5,8 @@ import { compose, withProps, lifecycle } from 'recompose'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import { StandaloneSearchBox } from 'react-google-maps/lib/components/places/StandaloneSearchBox'
 
+import InputSearch from 'vtex.styleguide/InputSearch'
+
 const SearchMap = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCUbzqhN6HZoty-UigCHG4bitF-Vl2GU7U&v=3.exp&libraries=geometry,drawing,places",
@@ -28,19 +30,20 @@ const SearchMap = compose(
       })
     },
   }),
-  withScriptjs
-)(props =>
+  withScriptjs  
+)(props =>  
   <div className="w-100">
-    <StandaloneSearchBox
-      ref={props.onSearchBoxMounted}
-      bounds={props.bounds}
-      onPlacesChanged={props.onPlacesChanged}
-    >
-      <input
-        type="text"
-        placeholder="Enter your address"
-      />
-    </StandaloneSearchBox>
+      <StandaloneSearchBox
+        ref={props.onSearchBoxMounted}
+        bounds={props.bounds}
+        onPlacesChanged={props.onPlacesChanged}
+      >
+        <InputSearch
+            type="text"
+            placeholder="Enter your address"
+            size="x-large"
+        />
+      </StandaloneSearchBox>
 
     {typeof props.place !== typeof undefined && (
       <Fragment>
