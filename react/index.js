6d98fe758f702934db64aa5b2190b7619f76b2ef
Guilherme Rodrigues <gadr90@gmail.com>
2018-10-19 12:01:14 -0300

    Add option to hide title and tabs

--- react/index.js
+++ react/index.js
@@ -27,47 +27,49 @@ class AddressLocator extends Component {
 
   render() {
     const { currentTab } = this.state
+    const { hideTabs, hideTitle } = this.props
+
+    const addressSearchTab = <FormattedMessage id="address-locator.address-search-tab" />
+    const addressRedeemTab = <FormattedMessage id="address-locator.address-redeem-tab" />
+    const addressSearch = (
+      <Query query={logisticsQuery}>
+        {({ data, loading }) => {
+          if (loading) {
+            return <Spinner />
+          }
+
+          const { googleMapsKey } = data.logistics
+
+          return (
+            <AddressSearch
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleMapsKey}&v=3.exp&libraries=places`}
+              googleMapKey={googleMapsKey}
+              loadingElement={<div className="h-100" />}
+            />
+          )
+        }}
+      </Query>
+    )
+
+    const tabs = (
+      <Tabs fullWidth>
+        <Tab label={addressSearchTab} active={currentTab === 1} onClick={() => this.handleTabChange(1)}>
+          {addressSearch}
+        </Tab>
+        <Tab label={addressRedeemTab} active={currentTab === 2} onClick={() => this.handleTabChange(2)}>
+          <AddressRedeem />
+        </Tab>
+      </Tabs>
+    )
 
     return (
       <div className="vtex-address-locator w-100 w-50-m center flex flex-column justify-center items-center pa6">
-        <Adopt mapper={{
-          title: <FormattedMessage id="address-locator.order-title" />,
-        }}>
-          {({ title }) => (
-            <span className="db b f1 mb7">{title}</span>
-          )}
-        </Adopt>
-        <Adopt mapper={{
-          addressSearchTab: <FormattedMessage id="address-locator.address-search-tab" />,
-          addressRedeemTab: <FormattedMessage id="address-locator.address-redeem-tab" />,
-        }}>
-          {({ addressSearchTab, addressRedeemTab }) => (
-            <Tabs fullWidth>
-              <Tab label={addressSearchTab} active={currentTab === 1} onClick={() => this.handleTabChange(1)}>
-                <Query query={logisticsQuery}>
-                  {({ data, loading }) => {
-                    if (loading) {
-                      return <Spinner />
-                    }
-
-                    const { googleMapsKey } = data.logistics
-
-                    return (
-                      <AddressSearch
-                        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleMapsKey}&v=3.exp&libraries=places`}
-                        googleMapKey={googleMapsKey}
-                        loadingElement={<div className="h-100" />}
-                      />
-                    )
-                  }}
-                </Query>
-              </Tab>
-              <Tab label={addressRedeemTab} active={currentTab === 2} onClick={() => this.handleTabChange(2)}>
-                <AddressRedeem />
-              </Tab>
-            </Tabs>
-          )}
-        </Adopt>
+        {!hideTitle && (
+          <h1 className="db b f1 mb7">
+            <FormattedMessage id="address-locator.order-title" />
+          </h1>
+        )}
+        {hideTabs ? addressSearch : tabs}
       </div>
     )
   }
