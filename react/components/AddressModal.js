cc16a505af242ad53003455c6c857648f51627a6
Athos Couto <athos.couto@vtex.com.br>
2018-11-05 17:04:09 -0200

    Add mandatory address modal when there's no address

---
+++ react/components/AddressModal.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { FormattedMessage, intlShape } from 'react-intl'
+import { compose } from 'recompose'
+import { withRuntimeContext } from 'render'
+import PropTypes from 'prop-types'
+import { orderFormConsumer, contextPropTypes } from 'vtex.store/OrderFormContext'
+import { Modal } from 'vtex.styleguide'
+
+import AddressSearch from './Search'
+import AddressRedeem from './Redeem'
+import '../global.css'
+
+/**
+ * Component that allows the user to locate his address, by inserting, searching, retrieving and
+ * saving it into orderform.
+ * Configure the key for Google Geolocation API, by inserting it on the admin logistics section.
+ */
+class AddressModal extends Component {
+  static propTypes = {
+    /* Context used to call address mutation and retrieve the orderForm */
+    orderFormContext: contextPropTypes,
+    pageToRedirect: PropTypes.string,
+    runtime: PropTypes.shape({
+      page: PropTypes.string.isRequired,
+      pages: PropTypes.object.isRequired,
+    }).isRequired,
+  }
+
+  static defaultProps = { pageToRedirect: 'store/order' }
+
+  static contextTypes = { intl: intlShape }
+
+  state = {
+    isOpen: false,
+  }
+
+  needAddress() {
+    const shippingData = this.props.orderFormContext.orderForm.shippingData
+    return shippingData == null || shippingData.address == null
+  }
+
+  handleClose = () => this.setState({ isOpen: false })
+
+  /* Function that will be called when updating the orderform */
+  handleOrderFormUpdated = async () => await this.props.orderFormContext.refetch()
+
+  render() {
+    return (
+      <Modal {...{ isOpen: this.needAddress(), closeOnEsc: false, closeOnOverlayClick: false, showCloseIcon: false, onClose: () => {} }} >
+        <h1 className="db b f1 mb7 mt0 pa5 tl tc-m">
+          <FormattedMessage id="address-locator.order-title" />
+        </h1>
+        <div className="vtex-address-locator w-100 w-50-m center flex flex-column justify-center items-center pa6">
+          <AddressSearch
+            orderFormContext={this.props.orderFormContext}
+            onOrderFormUpdated={this.handleOrderFormUpdated}
+          />
+          <AddressRedeem
+            orderFormContext={this.props.orderFormContext}
+            onOrderFormUpdated={this.handleOrderFormUpdated}
+          />
+        </div>
+      </Modal>
+    )
+  }
+}
+
+export default compose(
+  withRuntimeContext,
+  orderFormConsumer
+)(AddressModal)
---
+++ react/components/ChangeAddressModal.js
@@ -0,0 +1,132 @@
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { FormattedMessage } from 'react-intl'
+import _ from 'lodash' //TODO: Replace with ramda
+import { orderFormConsumer, contextPropTypes } from 'vtex.store/OrderFormContext'
+import Modal from 'vtex.styleguide/Modal'
+import Button from 'vtex.styleguide/Button'
+import Spinner from 'vtex.styleguide/Spinner'
+import NewAddressIcon from './NewAddressIcon'
+import AddressList from './AddressList'
+import AddressSearch from './Search'
+import '../global.css'
+
+/**
+ * Component responsible for displaying and managing user's address using orderFormContext.
+ */
+class ChangeAddressModal extends Component {
+  static propTypes = {
+    orderFormContext: contextPropTypes,
+    isOpen: PropTypes.bool,
+    onClose: PropTypes.func,
+  }
+
+  state = {
+    isSearchingAddress: false,
+    isLoading: false,
+  }
+
+  /* Filters available addresses and returns only valid ones */
+  getValidAvailableAddresses = availableAddresses =>
+    availableAddresses.filter(address => address.city && address.street && address.number)
+
+  /**
+   * Prepare available addresses to list, by removing invalid and duplicate ones, as well reversing and slicing
+   *
+   * @returns {Array} The available addresses array prepared to list
+   */
+  getAvailableAddresses = () => {
+    /* It will set the max length of available addresses array */
+    const maxAddressesQuantity = 5
+
+    let { availableAddresses } = this.props.orderFormContext.orderForm.shippingData
+
+    /* Remove invalid addresses from array and then reverse it, to be sorted by the last selected */
+    availableAddresses = this.getValidAvailableAddresses(availableAddresses).reverse()
+    /* Remove duplicate objects from array and then slice by the setted max length */
+    availableAddresses = _.uniqWith(availableAddresses, _.isEqual).slice(0, maxAddressesQuantity)
+
+    return availableAddresses
+  }
+
+  handleSelectAddress = address => {
+    const { orderFormContext } = this.props
+    const { orderFormId } = orderFormContext.orderForm
+
+    this.setState({ isLoading: true })
+
+    orderFormContext
+      .updateOrderFormShipping({
+        variables: {
+          orderFormId,
+          address,
+        },
+      })
+      .then(async () => await this.handleOrderFormUpdated())
+  }
+
+  handleCloseModal = () => {
+    this.props.onClose()
+    this.setState({
+      isSearchingAddress: false,
+      isLoading: false,
+    })
+  }
+
+  handleOrderFormUpdated = async () => {
+    const { orderFormContext } = this.props
+
+    await orderFormContext.refetch()
+    this.handleCloseModal()
+  }
+
+  handleAddressSearch = () => this.setState({ isSearchingAddress: true })
+
+  render() {
+    const { orderFormContext, isOpen } = this.props
+    const { shippingData } = orderFormContext.orderForm
+
+    if (!shippingData || !shippingData.address) return null
+
+    const { isSearchingAddress, isLoading } = this.state
+    const availableAddresses = this.getAvailableAddresses()
+
+    return (
+      <Modal isOpen={isOpen} onClose={this.handleCloseModal}>
+        <p className="f4 pa5 ma0 bb b--light-gray bw1 b near-black">
+          <FormattedMessage id="address-locator.address-manager-title" />
+        </p>
+        <span className={`${isSearchingAddress ? 'mt10' : 'mt8'} db mb5 tc`}>
+          <NewAddressIcon />
+        </span>
+        {!isSearchingAddress ? (
+          <Fragment>
+            <div className="pa5 mb5">
+              <Button onClick={this.handleAddressSearch} block>
+                <FormattedMessage id="address-locator.address-manager-button" />
+              </Button>
+            </div>
+            {!isLoading ? (
+              <AddressList
+                availableAddresses={availableAddresses}
+                onOrderFormUpdated={this.handleCloseModal}
+                onSelectAddress={this.handleSelectAddress}
+              />
+            ) : (
+                <i className="tc">
+                  <Spinner />
+                </i>
+              )}
+          </Fragment>
+        ) : (
+            <AddressSearch
+              onOrderFormUpdated={this.handleOrderFormUpdated}
+              orderFormContext={orderFormContext}
+            />
+          )}
+      </Modal>
+    )
+  }
+}
+
+export default ChangeAddressModal
