16862d47a9a63641b82a8f47ee9bb5ce60ee438b
Joao Fidelis <joaogfidelis@gmail.com>
2019-04-18 15:29:06 -0300

    remove comments

--- react/components/ChangeAddress/index.js
+++ react/components/ChangeAddress/index.js
@@ -146,44 +146,6 @@ class ChangeAddress extends Component {
         </div>
       </div>
     )
-
-    // return (
-    //   <Modal isOpen={isOpen} onClose={this.handleCloseModal} centered>
-    //     <div className="overflow-hidden relative br2"
-    //       style={{
-    //         margin: '-3rem',
-    //         padding: '3rem',
-    //       }}>
-    //       <div
-    //         style={{
-    //           transition: 'transform 300ms',
-    //           transform: `translate3d(${isPickupOpen ? '-100%' : '0'}, 0, 0)`
-    //         }}>
-    //         <AddressContent
-    //           onPickupClick={this.handlePickupClick}
-    //           onUpdateOrderForm={this.handleChangeAddress} />
-    //         {!isLoading ? (
-    //           <AddressList
-    //             availableAddresses={availableAddresses}
-    //             onOrderFormUpdated={this.handleOrderFormUpdated}
-    //             onSelectAddress={this.handleSelectAddress}
-    //           />
-    //         ) : (
-    //           <div className="flex flex-grow-1 justify-center items-center">
-    //             <Spinner />
-    //           </div>
-    //         )}
-    //       </div>
-    //       <div className="absolute w-100 h-100 top-0" style={{
-    //         left: '100%',
-    //         transition: 'transform 300ms',
-    //         transform: `translate3d(${isPickupOpen ? '-100%' : '0'}, 0, 0)`
-    //       }}>
-    //         {pickupPage}
-    //       </div>
-    //     </div>
-    //   </Modal>
-    // )
   }
 }
 
